87e37794701b1f7669a48ccbf06b523e67807d9a
Damian Mielcarekan <damian.mielcarek@gmail.com>
2016-11-17 17:57:28 +0100

    working in progress

---
+++ src/main/java/sample/service/WeatherService.java
@@ -0,0 +1,10 @@
+package sample.service;
+
+import sample.model.Weather;
+
+/**
+ * Created by Lenovo on 2016-11-17.
+ */
+public interface WeatherService {
+    Weather getWeatherFromUrl(String url);
+}
---
+++ src/main/java/sample/service/WeatherServiceJsonOrg.java
@@ -0,0 +1,14 @@
+package sample.service;
+
+import sample.model.Weather;
+
+/**
+ * Created by Lenovo on 2016-11-17.
+ */
+public class WeatherServiceJsonOrg implements WeatherService {
+
+
+    public Weather getWeatherFromUrl(String url) {
+        return null;
+    }
+}
