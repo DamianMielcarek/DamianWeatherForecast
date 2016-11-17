1ce8af84bf2ea18b9ae8c4299d63ee3fe504c3d1
Damian Mielcarekan <damian.mielcarek@gmail.com>
2016-11-17 17:48:54 +0100

    added json and commons dependencies

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -5,6 +5,12 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.apache.commons.io.IOUtils;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.Charset;
 
 public class Main extends Application {
 
@@ -18,6 +24,11 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
-        launch(args);
+        try {
+            JSONObject json = new JSONObject(IOUtils.toString(new URL("http://api.apixu.com/v1/current.json?key=94883fef4b9f4b2b9f7150333161711&q=Paris"), Charset.forName("UTF-8")));
+            System.out.println(json);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
