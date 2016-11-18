2875dd5021cba3ff8df76e12ec8f6e7e8c5932f0
Damian Mielcarekan <damian.mielcarek@gmail.com>
2016-11-18 13:35:06 +0100

    added searching cities functionality and Enter handling

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -1,8 +1,13 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import sample.model.Weather;
 import sample.service.WeatherServiceJsonOrg;
@@ -27,19 +32,38 @@ public class Controller implements Initializable {
     @FXML
     private Label cityLabel;
 
+    @FXML
+    private TextField cityTextField;
+
+    @FXML
+    private Button sendButton;
+
+    @FXML
+    public void onEnter (ActionEvent actionEvent) {
+        setValuesComponentsFromServer(cityTextField.getText());
+    }
+
     private Weather currentWeather;
 
     public void initialize(URL location, ResourceBundle resources) {
-        setValuesComponentsFromServer();
+        setValuesComponentsFromServer(null);
+        sendButton.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+                setValuesComponentsFromServer(cityTextField.getText());
+            }
+        });
     }
 
-    private void setValuesComponentsFromServer(){
+    private void setValuesComponentsFromServer(String myCity){
         WeatherServiceJsonOrg weatherServiceJsonOrg = new WeatherServiceJsonOrg("94883fef4b9f4b2b9f7150333161711");
-        currentWeather = weatherServiceJsonOrg.getWeatherFromUrl("http://api.apixu.com/v1/current.json", "Warsaw");
+        String city = myCity != null ? myCity : "Warsaw";
+        currentWeather = weatherServiceJsonOrg.getWeatherFromUrl("http://api.apixu.com/v1/current.json", city);
         dataLabel.setText(currentWeather.getLastUpdated());
         cityLabel.setText(currentWeather.getLocation());
         temperatureLabel.setText(String.valueOf(currentWeather.getTemperatureInCelsius()));
         textConditionLabel.setText(currentWeather.getWeatherCondition().getText());
+        Image image = new Image("http:"+currentWeather.getWeatherCondition().getIconUrl(), true);
+        iconImageView.setImage(image);
     }
 }
 
--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -17,8 +17,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Weather Forecast");
+        primaryStage.setScene(new Scene(root, 500, 500));
         primaryStage.show();
     }
 
